c263d5b21d23ca1c6db9259c9cc0abf706707669
HamaRegaya <regaya.mohamed@esprit.tn>
2024-10-23 20:31:13 +0100

    googlecloud initiation

---
+++ imagen_3.py
@@ -0,0 +1,19 @@
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+from google.cloud import aiplatform
+from dotenv import load_dotenv
+import os
+
+
+# aiplatform initiation
+load_dotenv()
+key_path = "credentials.json"
+credentials = Credentials.from_service_account_file(key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"])
+
+if credentials.expired:
+    credentials.refresh(Request())
+PROJECT_ID = os.getenv("GCP_PROJECT")
+LOCATION =  os.getenv('GCP_REGION')   
+aiplatform.init(project = PROJECT_ID,location=LOCATION, credentials=credentials)
+print("project id : "+PROJECT_ID+" location : "+LOCATION)
+
