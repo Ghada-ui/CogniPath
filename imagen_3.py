8e566a529248994044bd394a0248a8e8353b064d
HamaRegaya <regaya.mohamed@esprit.tn>
2024-10-23 20:34:44 +0100

    ImageGenerationModel: imagen-3.0-generate-001

--- imagen_3.py
+++ imagen_3.py
@@ -3,7 +3,8 @@ from google.oauth2.service_account import Credentials
 from google.cloud import aiplatform
 from dotenv import load_dotenv
 import os
-
+import vertexai
+from vertexai.preview.vision_models import ImageGenerationModel
 
 # aiplatform initiation
 load_dotenv()
@@ -17,3 +18,28 @@ LOCATION =  os.getenv('GCP_REGION')
 aiplatform.init(project = PROJECT_ID,location=LOCATION, credentials=credentials)
 print("project id : "+PROJECT_ID+" location : "+LOCATION)
 
+output_file = "my-output.png"
+prompt = "a magical landscape with a castle in the sky"
+
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-001")
+
+images = model.generate_images(
+    prompt=prompt,
+    # Optional parameters
+    number_of_images=1,
+    language="en",
+    # You can't use a seed value and watermark at the same time.
+    # add_watermark=False,
+    # seed=100,
+    aspect_ratio="1:1",
+    safety_filter_level="block_some"
+)
+
+images[0].save(location=output_file, include_generation_parameters=False)
+
+# Optional. View the generated image in a notebook.
+# images[0].show()
+
+print(f"Created output image using {len(images[0]._image_bytes)} bytes")
